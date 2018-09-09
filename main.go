1e57bcc85d0d0f4e480d348ec59fea803274d03f
lucifer <psinghal4210@gmail.com>
2018-09-10 00:42:58 +0530

    Function declaration and calling

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	//var card string = "Aces of Spades"
-	card := "Ace of Spades"
+	card := getCard()
 
 	fmt.Println(card)
+}
+
+func getCard() string {
+	return "Ace of Spades"
 }
