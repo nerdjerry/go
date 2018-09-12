54de1861dbfd2a945e9b7e3e4ba178ec79c8cc0e
lucifer <psinghal4210@gmail.com>
2018-09-12 21:56:33 +0530

    Create a new function that creates deck using loop, type, array of strings

--- deck.go
+++ deck.go
@@ -6,6 +6,19 @@ import (
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
 func (d deck) print() {
 	for i, value := range d {
 		fmt.Println(i, value)
