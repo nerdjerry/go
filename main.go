baec21432cb3c6d93c8db190982386a17172142c
lucifer <psinghal4210@gmail.com>
2018-09-12 21:56:59 +0530

    Use function to create new cards deck

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
 func main() {
-	//Create a slice of string
-	cards := deck{"Ace of Diamonds", getCard()}
-	//Add value to slice and expand its capacity
-	cards = append(cards, "6 of Club")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func getCard() string {
-	return "Ace of Spades"
 }
