f18296c94c67f0547d3379a07d62544296438f38
lucifer <psinghal4210@gmail.com>
2018-09-20 01:15:23 +0530

    Add test cases for checking end cards

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 	if len(deck) != 24 {
 		t.Errorf("Expected 30 cards but got %v", len(deck))
 	}
+
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades but got %v", deck[0])
+	}
+
+	if deck[len(deck)-1] != "Five of Clubs" {
+		t.Errorf("Expected Five of Clubs but got %v", deck[len(deck)-1])
+	}
 }
