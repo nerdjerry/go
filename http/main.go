e67f46ed7c81cb95ee21bcdd61751abf6bf97ba2
lucifer <psinghal4210@gmail.com>
2018-10-05 01:29:58 +0530

    Create a custom writer interface type and use with io.Copy

--- http/main.go
+++ http/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
 
-	io.Copy(os.Stdout, resp.Body)
+func (logWriter) Write(bs []byte) (n int, err error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just printed out this much bytes: ", len(bs))
+	return len(bs), nil
 }
